4c00d9b4171e844a55416e1129856294bc941f68
Sidney S <sandsidn@badinhs.org>
2016-09-20 08:04:47 -0400

    Initial Commit

---
+++ SwiftAssess1a/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAssess1a
+//
+//  Created by BHSRam9 on 9/20/16.
+//  Copyright © 2016 BHSRam9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
