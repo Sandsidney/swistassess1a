b067ef0085fd29b29062e9c78fdf92628a35f678
Sidney S <sandsidn@badinhs.org>
2016-09-20 21:00:11 -0400

    First Commit

--- SwiftAssess1a/ViewController.swift
+++ SwiftAssess1a/ViewController.swift
@@ -1,10 +1,10 @@
-//
-//  ViewController.swift
-//  SwiftAssess1a
-//
-//  Created by BHSRam9 on 9/20/16.
-//  Copyright © 2016 BHSRam9. All rights reserved.
-//
+/*
+ViewController.swift
+SwiftAssess1a
+
+Created by BHSRam9 on 9/20/16.
+Copyright © 2016 BHSRam9. All rights reserved.
+*/
 
 import UIKit
 
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
     }
 
+    
+    @IBOutlet var myLabel2: [UILabel]!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
